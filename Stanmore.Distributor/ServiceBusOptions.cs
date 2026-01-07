50503f8a2409508c62a7b32a2397a2c8db7a83f3
Kinaetron <darren.daley@hotmail.co.uk>
2026-01-07 11:22:57 +0000

    wrote the code that actually does the work

---
+++ Stanmore.Distributor/ServiceBusOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace Stanmore.Distributor;
+
+public class ServiceBusOptions
+{
+    public required string ConnectionString { get; set; }
+    public required string DeleteJourneys { get; set; }
+}
